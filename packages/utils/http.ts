484f3ff6ab23e44395d090323b7f533c832ec727
roushou <roushou9@gmail.com>
2024-06-23 03:21:40 +0700

    feat(utils): add http `get` method

--- packages/utils/http.ts
+++ packages/utils/http.ts
@@ -2,6 +2,21 @@ const baseHeaders: HeadersInit = {
   "Content-Type": "application/json",
 };
 
+export async function get<TResponse>(
+  url: string,
+  options?: Omit<RequestInit, "method" | "body">,
+): Promise<TResponse> {
+  const resp = await fetch(url, {
+    method: "GET",
+    ...options,
+    headers: {
+      ...baseHeaders,
+      ...options?.headers,
+    },
+  });
+  return (await resp.json()) as TResponse;
+}
+
 export async function post<TResponse>(
   url: string,
   options?: Omit<RequestInit, "method">,
